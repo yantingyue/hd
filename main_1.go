4915893f6e02e32f720fb3135689d9cd6549b311
yantingyue <784293715@qq.com>
2023-08-30 21:03:26 +0800

    hd

--- main_1.go
+++ main_1.go
@@ -34,7 +34,7 @@ type ResponseData struct {
 	Code int    `json:"code"`
 	Msg  string `json:"msg"`
 	Data []struct {
-		OrderID    uint64 `json:"order_id"`
+		OrderID    int    `json:"order_id"`
 		Title      string `json:"title"`
 		Picture    string `json:"picture"`
 		UpdateTime string `json:"update_time"`
@@ -45,67 +45,81 @@ type ResponseData struct {
 }
 
 func main() {
-	for {
-		if b == 1 {
-			Fj()
-		}
-		if b == 2 {
-			Rp()
-		}
-	}
+
+	go Fj()
+	select {}
 }
 
 const (
-	b                 = 1                                  //1是分解 2是置换
-	actId             = 532                                //活动id
-	tokenYanTingYue   = "24b9fe58d01f4374be37623c36f48f2a" //颜庭跃
+	b                 = 2                                  //1是分解 2是置换
+	actId             = 535                                //活动id
+	thread            = 2                                  //并发数
+	tokenCommon       = "3662106dd9b749d3995348c5f2884a5b" //勿删
+	tokenYanTingYue   = "3662106dd9b749d3995348c5f2884a5b" //颜庭跃
 	tokenYanTingYueDa = "24715fa709414f6eb364ffb6f8c13485" //颜庭跃
+
 )
 
 func Fj() {
-	orderInfo := GetOrderId(actId, tokenYanTingYue)
-	if orderInfo.Code == 0 && orderInfo.Msg == "success" && len(orderInfo.Data) > 0 {
+	go func() {
 		for {
-			if FjDetail(actId, tokenYanTingYue) {
-				break
+			//分解
+			if b == 1 {
+				for i := 0; i < thread; i++ {
+					go func() {
+						if FjDetail(actId, tokenCommon) {
+							//颜庭跃
+							go func() {
+								if len(tokenYanTingYue) > 0 {
+									//查看订单详情
+									orderId := GetOrderId(actId, tokenYanTingYue)
+									if orderId > 0 {
+										Replace(actId, orderId, tokenYanTingYue)
+									}
+								}
+							}()
+						}
+					}()
+					time.Sleep(time.Millisecond * 20)
+				}
+			}
+			//置换
+			if b == 2 {
+				for i := 0; i < thread; i++ {
+					go func() {
+						if ReplaceDetail(actId, tokenCommon) {
+							Replace(actId, 206110329, tokenYanTingYue)
+						}
+					}()
+				}
+				time.Sleep(time.Millisecond * 20)
+
 			}
 		}
-		for _, v := range orderInfo.Data {
-			go Replace(actId, v.OrderID, tokenYanTingYue)
-			time.Sleep(time.Millisecond * 2000)
-		}
-	}
-	time.Sleep(time.Millisecond * 2000)
+	}()
+
 }
 
-func Rp() {
-	orderInfo := GetOrderId(actId, tokenYanTingYue)
-	if orderInfo.Code == 0 && orderInfo.Msg == "success" && len(orderInfo.Data) > 0 {
-		for {
-			if ReplaceDetail(actId, tokenYanTingYue) {
-				break
-			}
-		}
-		for _, v := range orderInfo.Data {
-			go Replace(actId, v.OrderID, tokenYanTingYue)
-			time.Sleep(time.Millisecond * 2000)
-		}
-	}
-	time.Sleep(time.Millisecond * 2000)
-}
-
-func GetOrderId(id uint64, token string) (res ResponseData) {
+func GetOrderId(id uint64, token string) uint64 {
 	header := GenerateHeader1(token)
 	body := map[string]interface{}{
 		"replace_id": id,
 		"pageNumber": 1,
-		"pageSize":   5,
+		"pageSize":   20,
 	}
 	jsonBytes, _ := json.Marshal(body)
 	resp, _ := Post("https://api.aichaoliuapp.cn/aiera/ai_match_trading/nft/combination/choice/material", header, jsonBytes)
 	log.Println(string(resp))
+	if len(resp) == 0 {
+		return 0
+	}
+	res := ResponseData{}
 	json.Unmarshal(resp, &res)
-	return res
+	if res.Code == 0 && res.Msg == "success" && len(res.Data) > 0 {
+		return uint64(res.Data[0].OrderID)
+	}
+	return 0
+
 }
 
 func FjDetail(id uint64, token string) bool {
@@ -159,7 +173,7 @@ func ReplaceDetail(id uint64, token string) bool {
 	//resDetail.Data.StartTimeTimestamp = 1689605160000
 	diffTime := resDetail.Data.StartTimeTimestamp - resTime.CurrentMilliTime
 	log.Println(resDetail.Data.StartTimeTimestamp, resTime.CurrentMilliTime, diffTime)
-	if diffTime < 100 && diffTime > 0 {
+	if diffTime < 150 && diffTime > 0 {
 		//time.Sleep(time.Millisecond * time.Duration(diffTime))
 		log.Println(diffTime, resTime.CurrentMilliTime, resDetail.Data.StartTimeTimestamp, time.Now().UnixMilli())
 		return true
